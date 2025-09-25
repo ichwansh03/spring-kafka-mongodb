ef0f3e490cf1fd3ed2a44dd59ee9cc592403a3bf
ichwansh03 <ichwansholihin70@gmail.com>
2025-09-26 06:00:41 +0700

    add consumer factory

--- src/main/java/com/ichwan/message/LogEntryConfig.java
+++ src/main/java/com/ichwan/message/LogEntryConfig.java
@@ -1,16 +1,18 @@
 package com.ichwan.message;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.config.TopicConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaAdmin;
-import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.core.RoutingKafkaTemplate;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
@@ -18,6 +20,7 @@ import java.util.Map;
 import java.util.regex.Pattern;
 
 @Configuration
+@EnableKafka
 public class LogEntryConfig {
 
     @Value("${spring.kafka.bootstrap-servers}")
@@ -26,8 +29,14 @@ public class LogEntryConfig {
     @Value("${kafka.topic.log-entry}")
     private String logEntriesTopic;
 
+    @Value("${spring.kafka.consumer.group-id.log-entry}")
+    private String logEntryGroupId;
+
     @Value("${kafka.topic.mongo-data}")
     private String mongoDataTopic;
+
+    @Value("${spring.kafka.consumer.group-id.mongo-data}")
+    private String mongoDataGroupId;
 
     @Bean
     public KafkaAdmin.NewTopics logTopic() {
@@ -67,4 +76,29 @@ public class LogEntryConfig {
         return new RoutingKafkaTemplate(factoryMap);
     }
 
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, LogEntry> mongoDataKafkaListenerContainerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrap);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, mongoDataGroupId);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        DefaultKafkaConsumerFactory<String, LogEntry> consumerFactory = new DefaultKafkaConsumerFactory<>(props);
+        ConcurrentKafkaListenerContainerFactory<String, LogEntry> container = new ConcurrentKafkaListenerContainerFactory<>();
+        container.setConsumerFactory(consumerFactory);
+        return container;
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> logEntryKafkaListenerContainerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrap);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, logEntryGroupId);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        DefaultKafkaConsumerFactory<String, String> consumerFactory = new DefaultKafkaConsumerFactory<>(props);
+        ConcurrentKafkaListenerContainerFactory<String, String> container = new ConcurrentKafkaListenerContainerFactory<>();
+        container.setConsumerFactory(consumerFactory);
+        return container;
+    }
 }
